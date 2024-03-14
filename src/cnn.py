915b11408738cd8b877cb14cb341f588897ce084
knottwill <81773569+knottwill@users.noreply.github.com>
2024-03-14 11:06:32 +0000

    implemented cnn

---
+++ src/cnn.py
@@ -0,0 +1,122 @@
+"""
+We create a simple 2D convolutional neural network. This network
+is essentially going to try to estimate the diffusion process --- we
+can then use this network to generate realistic images.
+
+First, we create a single CNN block which we will stack to create the
+full network. We use `LayerNorm` for stable training and no batch dependence.
+
+We then create the full CNN model, which is a stack of these blocks
+according to the `n_hidden` tuple, which specifies the number of
+channels at each hidden layer.
+"""
+
+import torch
+import torch.nn as nn
+import numpy as np
+
+class CNNBlock(nn.Module):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        *, # Enforce following parameters to be passed as keywords
+        expected_shape, 
+        act=nn.GELU,
+        kernel_size=7,
+    ):
+        super().__init__()
+
+        self.net = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size, padding=kernel_size // 2),
+            nn.LayerNorm(expected_shape),
+            act()
+        )
+
+    def forward(self, x):
+        return self.net(x)
+    
+
+class CNN(nn.Module):
+    def __init__(
+        self,
+        in_channels,
+        expected_shape=(28, 28), # is this right? Do I need to include the channel dimension?
+        n_hidden=(64, 128, 64),
+        kernel_size=7,
+        last_kernel_size=3,
+        time_embeddings=16,
+        act=nn.GELU,
+    ) -> None:
+        
+        super().__init__()
+        last = in_channels
+
+        self.blocks = nn.ModuleList()
+        for hidden in n_hidden:
+            self.blocks.append(
+                CNNBlock(
+                    last,
+                    hidden,
+                    expected_shape=expected_shape,
+                    kernel_size=kernel_size,
+                    act=act,
+                )
+            )
+            last = hidden
+
+        # The final layer, we use a regular Conv2d to get the
+        # correct scale and shape (and avoid applying the activation)
+        self.blocks.append(
+            nn.Conv2d(
+                last,
+                in_channels,
+                last_kernel_size,
+                padding=last_kernel_size // 2,
+            )
+        )
+
+        ## This part is literally just to put the single scalar "t" into the CNN
+        ## in a nice, high-dimensional way:
+        self.time_embed = nn.Sequential(
+            nn.Linear(time_embeddings * 2, 128), act(),
+            nn.Linear(128, 128), act(),
+            nn.Linear(128, 128), act(),
+            nn.Linear(128, n_hidden[0]),
+        )
+        frequencies = torch.tensor(
+            [0] + [2 * np.pi * 1.5**i for i in range(time_embeddings - 1)]
+        )
+        self.register_buffer("frequencies", frequencies)
+
+    def time_encoding(self, t: torch.Tensor) -> torch.Tensor:
+
+        phases = torch.concat(
+            (
+                torch.sin(t[:, None] * self.frequencies[None, :]),
+                torch.cos(t[:, None] * self.frequencies[None, :]) - 1,
+            ),
+            dim=1,
+        )
+
+        return self.time_embed(phases)[:, :, None, None]
+
+    def forward(self, x: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
+        # Shapes of input:
+        #    x: (batch, chan, height, width)
+        #    t: (batch,) - scaled time t in [0, 1]
+        # assert t.min() >= 0 and t.max() <= 1, "t must be in [0, 1]"
+
+        embed = self.blocks[0](x)
+        # ^ (batch, n_hidden[0], height, width)
+
+        # Add information about time along the diffusion process
+        #  (Providing this information by superimposing in latent space)
+        embed += self.time_encoding(t)
+        #         ^ (batch, n_hidden[0], 1, 1) - thus, broadcasting
+        #           to the entire spatial domain
+
+        for block in self.blocks[1:]:
+            embed = block(embed)
+
+        return embed
