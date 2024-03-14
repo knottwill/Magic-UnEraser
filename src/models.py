a02b92f80d88c6321efac10e669d856f3bf1280e
knottwill <81773569+knottwill@users.noreply.github.com>
2024-03-14 11:09:59 +0000

    implemented DDPM diffusion model

---
+++ src/models.py
@@ -0,0 +1,60 @@
+from typing import Dict
+import torch
+import torch.nn as nn
+
+
+class DDPM(nn.Module):
+    def __init__(
+        self,
+        net,
+        noise_schedule: Dict[str, torch.Tensor],
+        criterion: nn.Module = nn.MSELoss(),
+    ) -> None:
+        super().__init__()
+
+        self.net = net
+
+        # `register_buffer` will track these tensors for device placement, but
+        # not store them as model parameters. This is useful for constants.
+        self.register_buffer("beta_t", noise_schedule["beta_t"])
+        self.beta_t  # Exists! Set by register_buffer
+        self.register_buffer("alpha_t", noise_schedule["alpha_t"])
+        self.alpha_t
+
+        self.T = len(self.beta_t) - 1 # beta_0 is not used
+        self.criterion = criterion
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Algorithm 18.1 in Prince
+        x ~ (batch_size, 1, H W)
+        """
+
+        t = torch.randint(1, self.T + 1, (x.size(0),), device=x.device)
+        eps = torch.randn_like(x)  # eps ~ N(0, 1)
+        alpha_t = self.alpha_t[t, None, None, None]  # Get right shape for broadcasting
+
+        z_t = torch.sqrt(alpha_t) * x + torch.sqrt(1 - alpha_t) * eps
+        # This is the z_t, which is sqrt(alphabar) x_0 + sqrt(1-alphabar) * eps
+        # We should predict the "error term" from this z_t. Loss is what we return.
+
+        return self.criterion(eps, self.net(z_t, t / self.T))
+
+    def sample(self, n_sample: int, size, device) -> torch.Tensor:
+        """Algorithm 18.2 in Prince"""
+
+        _one = torch.ones(n_sample, device=device)
+        z_t = torch.randn(n_sample, *size, device=device)
+        for i in range(self.T, 0, -1):
+            alpha_t = self.alpha_t[i]
+            beta_t = self.beta_t[i]
+
+            # First line of loop:
+            z_t -= (beta_t / torch.sqrt(1 - alpha_t)) * self.net(z_t, (i/self.T) * _one)
+            z_t /= torch.sqrt(1 - beta_t)
+
+            if i > 1:
+                # Last line of loop:
+                z_t += torch.sqrt(beta_t) * torch.randn_like(z_t)
+            # (We don't add noise at the final step - i.e., the last line of the algorithm)
+
+        return z_t
